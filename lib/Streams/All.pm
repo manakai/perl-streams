b6703f327a887cb9cdec434ea717d8e72d019df0
Wakaba <wakaba@suikawiki.org>
2022-03-17 15:01:21 +0900

    for convinience

---
+++ lib/Streams/All.pm
@@ -0,0 +1,20 @@
+package Streams::All;
+use strict;
+use warnings;
+
+use ArrayBuffer;
+use TypedArray;
+use DataView;
+use ReadableStream;
+use WritableStream;
+use Streams::Devel;
+use Streams::Error;
+use Streams::Filehandle;
+use Streams::IOError;
+use Streams::RangeError;
+use Streams::TypeError;
+use Streams::_Common;
+
+1;
+
+## License: Public Domain.
